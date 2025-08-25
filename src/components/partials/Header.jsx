1cb7514c19bcbf2b663fd05fb7de4d02254afc0d
Saad Raza <razasaad118@gmail.com>
2025-08-25 16:17:01 +0500

    router update

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -13,43 +13,31 @@ const Header = () => {
 
 
   return (
-     <BrowserRouter basename="/Porfolio-v3"> 
-        <React.Fragment>
-          <header className="header py-[1.875rem] flex container">
-            <div className="logo">
-              <Link to="/">
-                <img src={Logo} alt="" />
-              </Link>
-            </div>
-            <div className="nav flex gap-[4.597rem] ml-auto">
-              <ul className="hidden md:flex justify-center items-center gap-[3.597rem] w-fit">
-                <li><Link className="link" to="/">home</Link></li>
-                <li><Link className="link" to="/portfolio">portfolio</Link></li>
-                <li><Link className="link" to="/work">work</Link></li>
-                <li><Link className="link" to="/contact">contact</Link></li>
-                <li>
-                  <PrimaryButton
-                    text="resume"
-                    styleName="primary-button"
-                    link={process.env.RESUME}
-                  />
-                </li>
-              </ul>
-              <div className="switcher flex justify-center items-center">
-                <Switcher />
-              </div>
-            </div>
-          </header>
-          <Line />
+    <React.Fragment>
+      <header className='header py-[1.875rem] flex container'>
+          <div className='logo'>
+            <Link  to="/"><img src={Logo} alt='' /></Link>             
+          </div>
+          <div className='nav flex gap-[4.597rem] ml-auto'>
+              <ul className='hidden md:flex justify-center items-center gap-[3.597rem]  w-fit'>
+                  <li><Link className="link" to="/">home</Link></li>
+                  <li><Link className="link" to="/portfolio">portfolio</Link></li>
+                  <li><Link className="link" to="/work">work</Link></li>
+                  <li><Link className="link" to="/contact">contact  </Link></li>
+                  <li> <PrimaryButton text="resume" styleName='primary-button' link={process.env.RESUME} /></li>
+              </ul>              
+              <div className='switcher flex justify-center items-center'>
+                <Switcher/>
+             </div>
+          </div>          
+      </header>
+      <Line/>
+     <Routes basename="/Porfolio-v3">
+        <Route path="/"  element={<Home />} /> 
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
           
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </React.Fragment>
-    </BrowserRouter>
+    </React.Fragment>
   )
 }
 
